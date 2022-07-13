9f241403257312c7c021b4b49b65e465da7cdb8f
Juliusz Stasiewicz <juliusz.stasiewicz@scylladb.com>
2022-07-13 13:59:52 +0200

    generation_type: add (de)stringification functions

--- sstables/generation_type.cc
+++ sstables/generation_type.cc
@@ -7,16 +7,58 @@
  */
 
 #include "sstables/generation_type.hh"
+#include <cctype>
 #include <fmt/core.h>
 #include <cstdint>
+#include <iterator>
 #include <stdexcept>
+#include <string>
+#include <vector>
 
 namespace sstables {
 
+namespace {
+
+sstring to_base36(uint64_t i) {
+  std::vector<char> buf(13, '\0');
+  auto s = buf.begin();
+  do {
+    *s = "0123456789abcdefghijklmnopqrstuvwxyz"[-(i % 36)];
+    i /= 36;
+    s++;
+  } while (i);
+
+  return sstring(std::make_reverse_iterator(s), buf.rend());
+}
+
+uint64_t from_base36(const sstring& buf) {
+    uint64_t total = 0;
+    for (auto i = buf.size() - 1; i >= 0; --i) {
+        total *= 36;
+        const char c = std::tolower(buf[i]);
+        total += (c >= 'a' && c <= 'z') ? c - 'a' + 10 : c - '0';
+    }
+    return total;
+}
+
+} // anon. namespace
+
 sstring to_string(const generation_type& gen) {
-    return std::visit([] (const auto &x) {
-        return fmt::format("{}", x);
-    }, gen);
+    return std::visit(make_visitor(
+        [] (int64_t x) { return fmt::format("{}", x); },
+        [] (utils::UUID u) { 
+            auto ts = u.timestamp();
+            const auto nano_part = ts % 10'000'000;
+            ts /= 10'000'000;
+            const auto seconds = ts % 86'400;
+            ts /= 86'400;
+            return fmt::format("{:0>4s}_{:0>4s}_{:0>5s}{:0>13s}",
+                to_base36(ts),
+                to_base36(seconds),
+                to_base36(nano_part),
+                to_base36(u.get_least_significant_bits()));
+        }
+    ), gen);
 }
 
 int calc_shard_num(const generation_type& gen) {
@@ -27,7 +69,20 @@ int calc_shard_num(const generation_type& gen) {
 }
 
 generation_type generation_from_string(const sstring& str) {
-    throw std::runtime_error("unimplemented");
+    if (str.size() == 28) {
+        // UUID-based generation is 28 chars long
+        const auto days = from_base36(str.substr(0, 4));
+        const auto seconds = from_base36(str.substr(5, 4));
+        const auto nano_part = from_base36(str.substr(10, 5));
+
+        const auto lsb = from_base36(str.substr(15, 13));
+        const auto msb = ((days * 86'400) + seconds) * 10'000'000 + nano_part;
+
+        return utils::UUID(msb, lsb);
+    } else {
+        // int-based (legacy) generation
+        return std::stoll(str);
+    }
 }
 
 generation_type increment(const generation_type& gen) {
